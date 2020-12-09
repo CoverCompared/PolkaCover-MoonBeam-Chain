1c34ca23f6e4167c4a2178aca42b7f78f7958d8a
“coderasha” <ashadipta@gmail.com>
2020-12-09 14:00:00 +0100

    MoonBeam Chain

---
+++ scripts/init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+source $HOME/.cargo/env
+
+echo "*** Initializing WASM build environment"
+
+RUST_NIGHTLY_VERSION=$(cat rust-toolchain)
+
+if [ -z ${WASM_BUILD_TOOLCHAIN+x} ]; then
+  WASM_BUILD_TOOLCHAIN=$RUST_NIGHTLY_VERSION
+fi
+
+if [ -z $CI_PROJECT_NAME ] ; then
+  rustup update $WASM_BUILD_TOOLCHAIN
+  rustup update stable
+fi
+
+rustup target add wasm32-unknown-unknown --toolchain $WASM_BUILD_TOOLCHAIN
